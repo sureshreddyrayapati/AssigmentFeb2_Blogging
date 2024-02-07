524f1e4f15f61a1d751ae0db11c8c83983b13d31
sureshreddyrayapati <resuresh0@gmail.com>
2024-02-07 07:57:53 +0530

    Add project files.

---
+++ AssigmentFeb2/Models/Blog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AssigmentFeb2.Models
+{
+        [Table("Post")]
+    public class Blog
+    {
+        [Key]
+        public int Id {  get; set; }
+        public string Titile { get; set; }
+        public string Content { get; set; }
+        public DateTime PublicatiobDate { get; set; }
+        public string Author { get; set; }
+    }
+}
